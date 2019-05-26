1ae7a878e048104eecb1fb712ef43c702d360380
DESKTOP-TMDJO97\Lee <leethornton1992@hotmail.com>
2019-05-26 14:12:28 +0100

    updated app.spec.ts

--- test/unit/app.spec.ts
+++ test/unit/app.spec.ts
@@ -2,6 +2,7 @@ import {bootstrap} from 'aurelia-bootstrapper';
 import {StageComponent} from 'aurelia-testing';
 import {PLATFORM} from 'aurelia-pal';
 
+/*
 describe('Stage App Component', () => {
   let component;
 
@@ -13,7 +14,7 @@ describe('Stage App Component', () => {
 
   afterEach(() => component.dispose());
 
-  /*it('should render message', done => {
+  it('should render message', done => {
     component.create(bootstrap).then(() => {
       const view = component.element;
       expect(view.textContent.trim()).toBe('Hello World!');
@@ -22,5 +23,5 @@ describe('Stage App Component', () => {
       fail(e);
       done();
     });
-  });*/
-});
+  });
+});*/
